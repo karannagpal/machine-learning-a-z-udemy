a608dac5098276efd063baee5170eec945a4ea3c
Karan <karannagpal96@gmail.com>
2019-07-14 02:02:25 +0530

    Support vector regression

---
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr_kr.R
@@ -0,0 +1,27 @@
+# SVR
+
+# importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into the Training set and Test set
+# # install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting the Regression Model to the dataset
+# Create your regressor here
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the Regression Model results
+# install.packages('ggplot2')
+library(ggplot2)
